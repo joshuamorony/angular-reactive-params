d3224e24f5587f0adc234b63f23c9f58d4df2174
Joshua Morony <joshua.morony@gmail.com>
2022-05-03 14:12:30 +0930

    reactive example

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { User, UsersService } from '../shared/users.service';
 
 @Component({
@@ -7,19 +8,13 @@ import { User, UsersService } from '../shared/users.service';
   templateUrl: './detail.page.html',
   styleUrls: ['./detail.page.scss'],
 })
-export class DetailPage implements OnInit {
-  public user: User;
+export class DetailPage {
+  public user$ = this.route.paramMap.pipe(
+    switchMap((params) => this.usersService.getUser(params.get('id')))
+  );
 
   constructor(
     private route: ActivatedRoute,
     private usersService: UsersService
   ) {}
-
-  ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-
-    this.usersService.getUser(id).subscribe((user) => {
-      this.user = user;
-    });
-  }
 }
